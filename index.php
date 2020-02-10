04604ffd1fb8d93c86a19492409abfa252bd4aa1
Stefan-Nilsson <contact.stefan.nilsson@gmail.com>
2020-02-10 12:29:07 +0100

    add validate email, empty field check, validate number

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 //this line makes PHP behave in a more strict way
 declare(strict_types=1);
-
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
+error_reporting(E_ALL);
 //we are going to use session variables so we need to enable sessions
 session_start();
 
@@ -32,6 +34,92 @@ $products = [
     ['name' => 'Ice-tea', 'price' => 3],
 ];
 
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+// define variables and set to empty values
+
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+foreach ($_POST as $tempInput){
+
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["email"])) {
+        echo ($nameErr = "Mail is required </br>");
+    } else {
+        $name = test_input($_POST["email"]);
+    }
+
+    if (empty($_POST["street"])) {
+        echo($emailErr = "Street is required</br>");
+    } else {
+        $email = test_input($_POST["street"]);
+    }
+
+    if (empty($_POST["streetnumber"])) {
+        echo($website = "Streetnumber is required</br>");
+    } else {
+        $website = test_input($_POST["streetnumber"]);
+    }
+
+    if (empty($_POST["city"])) {
+        echo($comment = "City is required</br>");
+    } else {
+        $comment = test_input($_POST["city"]);
+    }
+
+    if (empty($_POST["zipcode"])) {
+        echo($genderErr = "Zipcode is required</br>");
+    } else {
+        $gender = test_input($_POST["zipcode"]);
+    }
+}
+
+
+
+
+
+function validateEmail($mailValidation){
+    return filter_var($mailValidation, FILTER_VALIDATE_EMAIL);
+}
+if (validateEmail($_POST['email'])){
+    echo ('gege');
+}
+else {
+    echo ('Valid email please');
+}
+
+
+$int = 100;
+function checkForNumber ($inputValue){
+    return filter_var($inputValue, FILTER_VALIDATE_INT);
+}
+if (checkForNumber($_POST['streetnumber'])){
+    echo('you have entered a valid number');
+}
+else {
+    echo('fill in a number in your street number');
+}
+
+if (checkForNumber($_POST['zipcode'])){
+    echo('you have entered a valid number');
+}
+else {
+    echo('fill in a number in your zipcode');
+}
+// echo($_POST['email']);
+
+whatIsHappening();
+
+
 $totalValue = 0;
 
+
 require 'form-view.php';
+
+
